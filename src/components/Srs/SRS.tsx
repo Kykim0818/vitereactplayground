09a99aecd5bf16c210d7793bb736bb3d78a25fee
Kykim0818 <Kykim@seegene.com>
2023-02-28 15:12:27 +0900

    진행상황 커밋

--- src/components/Srs/SRS.tsx
+++ src/components/Srs/SRS.tsx
@@ -1,4 +1,8 @@
 export const SRS = () => {
   // 대소문자 변경시에 문제가 없는지 확인해보자
+  // 처음에 변경 이력이 잡히지 않아,
+  // git config core.ignorecase false 를 하고 변경내역에 담아 push를했다.
+
+  // repo에 놀랍게도 SRS와 Srs의 두폴더 현재 모두 존재
   return <div>SRS</div>;
 };
